696a9ab05a63f2205e72d7ccb7a00f5f5f6bb9d9
amontalvof <amontalvof92@gmail.com>
2022-02-01 11:49:34 -0600

    feat: implemented tooltip in product buttons

--- src/components/GridCard/gridButtons.tsx
+++ src/components/GridCard/gridButtons.tsx
@@ -1,5 +1,6 @@
 import { FaHeart, FaEye, FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip';
 import { TStyle } from '../../interfaces/plantilla';
 import {
     EnlacesContainer,
@@ -15,53 +16,64 @@ interface IGridButtonsProps {
 const GridButtons = ({ tipo, plantillaStyles }: IGridButtonsProps) => {
     const isVirtual = tipo === 'virtual';
     return (
-        <EnlacesContainer className="col-xs-5">
-            <ButtonsContainer>
-                <StyledButton
-                    type="button"
-                    className="btn btn-default btn-xs"
-                    data-toggle="tooltip"
-                    title="Add to my wish list"
-                    style={{ outline: 'none' }}
-                    colorTexto={plantillaStyles?.colorTexto}
-                    colorFondo={plantillaStyles?.colorFondo}
-                >
-                    <IconContainer>
-                        <FaHeart />
-                    </IconContainer>
-                </StyledButton>
-                {isVirtual && (
+        <>
+            <EnlacesContainer className="col-xs-5">
+                <ButtonsContainer>
                     <StyledButton
                         type="button"
                         className="btn btn-default btn-xs"
-                        data-toggle="tooltip"
-                        title="Add to shopping cart"
+                        data-tip
+                        data-for="tooltipHeart"
                         style={{ outline: 'none' }}
                         colorTexto={plantillaStyles?.colorTexto}
                         colorFondo={plantillaStyles?.colorFondo}
                     >
                         <IconContainer>
-                            <FaShoppingCart />
+                            <FaHeart />
                         </IconContainer>
                     </StyledButton>
-                )}
-                <Link to="">
-                    <StyledButton
-                        type="button"
-                        className="btn btn-default btn-xs"
-                        data-toggle="tooltip"
-                        title="See product"
-                        style={{ outline: 'none' }}
-                        colorTexto={plantillaStyles?.colorTexto}
-                        colorFondo={plantillaStyles?.colorFondo}
-                    >
-                        <IconContainer>
-                            <FaEye />
-                        </IconContainer>
-                    </StyledButton>
-                </Link>
-            </ButtonsContainer>
-        </EnlacesContainer>
+                    {isVirtual && (
+                        <StyledButton
+                            type="button"
+                            className="btn btn-default btn-xs"
+                            data-tip
+                            data-for="tooltipCart"
+                            style={{ outline: 'none' }}
+                            colorTexto={plantillaStyles?.colorTexto}
+                            colorFondo={plantillaStyles?.colorFondo}
+                        >
+                            <IconContainer>
+                                <FaShoppingCart />
+                            </IconContainer>
+                        </StyledButton>
+                    )}
+                    <Link to="">
+                        <StyledButton
+                            type="button"
+                            className="btn btn-default btn-xs"
+                            data-tip
+                            data-for="tooltipEye"
+                            style={{ outline: 'none' }}
+                            colorTexto={plantillaStyles?.colorTexto}
+                            colorFondo={plantillaStyles?.colorFondo}
+                        >
+                            <IconContainer>
+                                <FaEye />
+                            </IconContainer>
+                        </StyledButton>
+                    </Link>
+                </ButtonsContainer>
+            </EnlacesContainer>
+            <ReactTooltip id="tooltipHeart" effect="solid">
+                Add to my wish list
+            </ReactTooltip>
+            <ReactTooltip id="tooltipCart" effect="solid">
+                Add to shopping cart
+            </ReactTooltip>
+            <ReactTooltip id="tooltipEye" effect="solid">
+                See product
+            </ReactTooltip>
+        </>
     );
 };
 
--- src/components/ListCard/listButtons.tsx
+++ src/components/ListCard/listButtons.tsx
@@ -1,5 +1,6 @@
 import { FaHeart, FaEye, FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import ReactTooltip from 'react-tooltip';
 import { TStyle } from '../../interfaces/plantilla';
 import { StyledButton, IconContainer, ButtonsContainer } from './styles';
 
@@ -12,51 +13,62 @@ const ListButtons = ({ tipo, plantillaStyles }: IListButtonsProps) => {
     const isVirtual = tipo === 'virtual';
 
     return (
-        <ButtonsContainer>
-            <StyledButton
-                type="button"
-                className="btn btn-default btn-xs"
-                data-toggle="tooltip"
-                title="Add to my wish list"
-                style={{ outline: 'none' }}
-                colorTexto={plantillaStyles?.colorTexto}
-                colorFondo={plantillaStyles?.colorFondo}
-            >
-                <IconContainer>
-                    <FaHeart />
-                </IconContainer>
-            </StyledButton>
-            {isVirtual && (
+        <>
+            <ButtonsContainer>
                 <StyledButton
                     type="button"
                     className="btn btn-default btn-xs"
-                    data-toggle="tooltip"
-                    title="Add to shopping cart"
+                    data-tip
+                    data-for="tooltipHeart"
                     style={{ outline: 'none' }}
                     colorTexto={plantillaStyles?.colorTexto}
                     colorFondo={plantillaStyles?.colorFondo}
                 >
                     <IconContainer>
-                        <FaShoppingCart />
+                        <FaHeart />
                     </IconContainer>
                 </StyledButton>
-            )}
-            <Link to="">
-                <StyledButton
-                    type="button"
-                    className="btn btn-default btn-xs"
-                    data-toggle="tooltip"
-                    title="See product"
-                    style={{ outline: 'none' }}
-                    colorTexto={plantillaStyles?.colorTexto}
-                    colorFondo={plantillaStyles?.colorFondo}
-                >
-                    <IconContainer>
-                        <FaEye />
-                    </IconContainer>
-                </StyledButton>
-            </Link>
-        </ButtonsContainer>
+                {isVirtual && (
+                    <StyledButton
+                        type="button"
+                        className="btn btn-default btn-xs"
+                        data-tip
+                        data-for="tooltipCart"
+                        style={{ outline: 'none' }}
+                        colorTexto={plantillaStyles?.colorTexto}
+                        colorFondo={plantillaStyles?.colorFondo}
+                    >
+                        <IconContainer>
+                            <FaShoppingCart />
+                        </IconContainer>
+                    </StyledButton>
+                )}
+                <Link to="">
+                    <StyledButton
+                        type="button"
+                        className="btn btn-default btn-xs"
+                        data-tip
+                        data-for="tooltipEye"
+                        style={{ outline: 'none' }}
+                        colorTexto={plantillaStyles?.colorTexto}
+                        colorFondo={plantillaStyles?.colorFondo}
+                    >
+                        <IconContainer>
+                            <FaEye />
+                        </IconContainer>
+                    </StyledButton>
+                </Link>
+            </ButtonsContainer>
+            <ReactTooltip id="tooltipHeart" effect="solid">
+                Add to my wish list
+            </ReactTooltip>
+            <ReactTooltip id="tooltipCart" effect="solid">
+                Add to shopping cart
+            </ReactTooltip>
+            <ReactTooltip id="tooltipEye" effect="solid">
+                See product
+            </ReactTooltip>
+        </>
     );
 };
 
